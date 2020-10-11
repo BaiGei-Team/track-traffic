5838f03cb7f9720629918c3dd796f473b1d1dcde
luowei135 <luowei135@jd.com>
2020-10-11 17:06:14 +0800

    替换注解

--- track-traffic-rpc/src/main/java/com/track/traffic/rpc/po/request/AlarmQueryRequest.java
+++ track-traffic-rpc/src/main/java/com/track/traffic/rpc/po/request/AlarmQueryRequest.java
@@ -13,15 +13,23 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class AlarmQueryRequest {
     /**
-     * 网络或设备名称，为空查询所有
+     * 要查询的网络名或设备名，为空时代表全网查询，“网络_192.168.1.123_161”
      */
     private String nodeName;
     /**
-     * 节点类型
+     * 表示要查询的是网络还是设备，为“NodeDevice”代表设备，为”NetWork”代表网 络，”NodeDevice”
      *
      * @see com.track.traffic.rpc.enums.NodeTypeEnum
      */
     private String nodeType;
+    /**
+     * 告警级别，8—掉线,7—严重,6—主要,5—次要,3—未知， 7,6
+     *
+     * @see com.track.traffic.rpc.enums.AlarmLevelEnum
+     */
     private Integer perceivedSeverity;
+    /**
+     * 告警类型，多个以逗号分隔，”CPU 负载,磁盘空间利用率”
+     */
     private String alarmType;
 }
