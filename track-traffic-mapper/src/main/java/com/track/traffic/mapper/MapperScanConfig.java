3d99f98bd82e43238d3d25013f71facfb96158fc
luowei135 <luowei135@jd.com>
2020-10-11 10:07:41 +0800

    支持扫描mapper

--- track-traffic-mapper/src/main/java/com/track/traffic/mapper/MapperScanConfig.java
+++ track-traffic-mapper/src/main/java/com/track/traffic/mapper/MapperScanConfig.java
@@ -1,6 +1,7 @@
 package com.track.traffic.mapper;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by SoulW on 2020/10/11.
@@ -8,6 +9,7 @@ import org.mybatis.spring.annotation.MapperScan;
  * @author SoulW
  * @since 2020/10/11 8:25
  */
+@Component
 @MapperScan("com.track.traffic.mapper")
 public class MapperScanConfig {
 }
